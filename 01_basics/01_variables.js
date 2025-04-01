6c1b8f5ae7e525cf8b3c6d64d9abf39088c67462
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-04-01 09:06:17 +0000

    datatypes basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId= "yogeshk7505"
+let accountPassword ="123456789"
+var accountEmail="yogi@gmail.com"
+accountCity="bhilai"
+let accountState;
+
+// accountId= "dsskfnadsf" not possible
+
+accountEmail="coding@gmial.ccom";
+accountPassword="43217648"
+
+console.log(accountCity);
+
+console.table([accountId, accountPassword,,accountEmail, accountCity,accountState])
+//console.table to [print everthing at once in the form of table
+
+
+/* do not use var 
+because of issue in block scope and fucntional scope 
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"//treats all js code as newer version
+
+//alert(3+3) //we are using nodejs , not the browser
+
+let name ="yogesh"
+let age= 20
+let isLoggedIn= true
+let state;
+
+//number=> 2 to the power 53
+// BigInt
+// string=>""
+// Boolean=>true/false
+// null=>standalone value
+// undefined=>
+// Symbol=>unique
+
+
+//object
+
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
