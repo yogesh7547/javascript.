b95df719ce7413922a8f89352b1adf7c766a7023
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-04-01 09:35:13 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score ="33acd"
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber= Number(score);
+//console.log(typeof valueInNumber);//now it says it has converted score into a number but it cannot since it has letters in it 
+
+//console.log(valueInNumber);// it will give  NaN in terminal, that is Not a Number
+
+
+//"33"=>33
+//"33abc"=>Nan
+//true=>1, false=>0
+//"adcas"=>NaN
+//null=>0
+//undefined=>NaN
+
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1=>true ;  0=>false
+//""=>false ;  
+//"yogi"=> true
+
+let someNumber= 33;
+let stringNumber= String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
