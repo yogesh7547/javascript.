545da2631498698f73819c970a8553a24bfb886a
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-04-04 10:16:36 +0000

    date and time

---
+++ 01_basics/07_date-and_time.js
@@ -0,0 +1,29 @@
+let myDate=new Date();
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+
+
+// console.log(typeof myDate);
+
+//let createDate= new Date(2003, 2, 12)
+//let createDate= new Date(2003, 2, 12,5 ,12)
+let createDate= new Date("2023-11-14");
+//console.log(createDate.toLocaleString()); 
+
+
+let myTimeStamp= Date.now()
+
+// console.log(myTimeStamp);
+// console.log(createDate.getTime());
+// console.log(Math.floor(Date.now()/1000)) //to convert the time in seconds 
+
+
+let newDate= new Date();
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth()+1);
