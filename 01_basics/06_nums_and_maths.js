711190f9797d19b2cf3069fe239b7a3768be9abc
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-04-04 09:39:08 +0000

    maths and numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+// const score=400;
+// console.log(score);
+
+// const balance= new Number(100);
+// console.log(balance);
+
+// //methods
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber= 123.894;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds= 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// **************************maths*****************
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.round(2.345));
+
+// console.log(Math.ceil(9.111));
+// console.log(Math.floor(9.111));
+// console.log(Math.max(1,2,3,4,5))
+// console.log(Math.min(1,2,3,4,5));
+
+console.log(Math.random());// gives a value between 0 and 1
+console.log(Math.floor(Math.random()*10)+1);
+
+const max= 20;
+const min=10;
+
+
+console.log(Math.floor((Math.random()*(max-min+1))+min))//formula to obtain a random value between some max and min limit 
